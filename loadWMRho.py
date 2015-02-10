cbfb5f9ecad7f80fd936070f7c62409c053125f1
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2015-02-10 10:27:41 -0500

    Created plots

--- loadWMRho.py
+++ loadWMRho.py
@@ -28,8 +28,7 @@ if __name__ == '__main__':
 	from rhoratefcns import findrho0
 	WM,names,dists,rbs,mubs,alphas,betas,gammas,M2Ls,MBH1s,MBH2s = getWM()
 	rho0s = findrho0(rbs,M2Ls,mubs)
-	ilist = array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
-       17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+	ilist = array([ 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
        35, 36, 37, 38, 39, 40]) #skip 21
 	for i in ilist:
 		print 'Working on ',names[i],' galaxy ',i+1,' of ',len(ilist)
