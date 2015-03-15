2c74ce88d48004b9209786147d3515f8b6b1490c
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2015-03-15 18:21:37 -0400

    Error output of problem galaxies added

--- runWMsmall.sh
+++ runWMsmall.sh
@@ -2,8 +2,8 @@
 #PBS -l nodes=1:ppn=1
 #PBS -q workq
 #PBS -l walltime=48:00:00
-#PBS -N  NGC3608
+#PBS -N  NGC4552
 cd $PBS_O_WORKDIR
 module load python/2.7.6
 export PYTHONPATH=$PATH:/mnt/scratch-lustre/njones/pyrate
-python WMrateget.py 48
+python WMrateget.py 49
