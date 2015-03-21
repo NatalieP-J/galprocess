606d2e2c04cd177e065739cf9b62a370d026bbb3
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2015-03-20 22:21:56 -0400

    MASTER FILES

--- plotallrates.py
+++ plotallrates.py
@@ -1,9 +1,17 @@
 from numpy import *
 import matplotlib.pyplot as plt
+import matplotlib
 import os
 from scipy.interpolate import interp1d
 from scipy.optimize import leastsq
 plt.ion()
+
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 18}
+
+matplotlib.rc('font', **font)
+
 
 def load(fname):
     f=open(fname,'r')
@@ -44,15 +52,20 @@ if __name__ == '__main__':
 	from rhomodels import NukerModelRho
 	from rhoratefcns import findrho0
 	from construction import pklread,displaycheck,existcheck
-	WM,names,dists,rbs,mubs,alphas,betas,gammas,M2Ls,MBH1s,MBH2s = getWM('NGC3115_vartab.dat')
-	os.chdir('/Users/Natalie/Data/Mar12WM')
+	WM,names,dists,rbs,mubs,alphas,betas,gammas,M2Ls,MBH1s,MBH2s = getWM('WM04.dat')
+	#os.chdir('/Users/Natalie/Data/Mar12WM')
 	rho0s = findrho0(rbs,M2Ls,mubs)
 	dcheck = displaycheck()
 	ilist = arange(0,50)
-	plt.figure()
 	colors = plt.get_cmap('spectral')(linspace(0, 1.0, len(ilist)))
+	#ilist = delete(ilist,11)
+	ilist = delete(ilist,5)
+	ilist = delete(ilist,5)
+	ilist = delete(ilist,26)
+	ilist = delete(ilist,46)
+	plt.figure()
 	rs = []
-	for i in range(len(WM)):#ilist:
+	for i in ilist:
 		print 'Working on ',names[i],' galaxy ',i+1,' of ',len(WM)
 		alpha = alphas[i]
 		beta = betas[i]
@@ -66,15 +79,16 @@ if __name__ == '__main__':
 		GENERATE = False
 		model1 = NukerModelRho(name1,alpha,beta,gamma,r0pc,rho0,MBH_Msun1,GENERATE,memo = False)
 		model2 = NukerModelRho(name2,alpha,beta,gamma,r0pc,rho0,MBH_Msun2,GENERATE,memo = False)
-		seton,plotdat = existcheck(model2.directory, dcheck)
-		if seton['dgdlnrp'] == 'OFF':
+		seton,plotdat = existcheck(model1.directory, dcheck)
+		if seton['dgdlnrp'] == 'OFF' and i != len(WM) - 1:
 			print 'plotting'
-			rate = pklread('{0}/dgdlnrp.pkl'.format(model2.directory))
-			plt.loglog(rate[:,0],rate[:,1],color = colors[i])
+			rate = pklread('{0}/dgdlnrp.pkl'.format(model1.directory))
+			plt.loglog(rate[:,0],rate[:,1],color = colors[i],linewidth = 3)#,label = '{0}'.format(log10(MBH_Msun2)))
 			linslope = (log10(rate[:,1][401]) - log10(rate[:,1][301]))/(log10(rate[:,0][401]) - log10(rate[:,0][301]))
 			#ratefn = interp1d(log10(rate[:,0]),log10(rate[:,1]))
-			#ind = getind(rate[:,0],1e-2)
-			ind = getind(rate[:,0],1e-4)
+			ind = getind(rate[:,0],1e-2)
+			#ind = 501
+			#ind = getind(rate[:,0],1e-4)
 			#slope = copy(linslope)
 			slopes = []
 			for k in range(ind,len(rate[:,0])-1):
@@ -86,12 +100,17 @@ if __name__ == '__main__':
 					mslope = j
 			#for l in range(len(slopes)-1):
 			#	print (slopes[l+1] - slopes[l])/(log10(rate[:,0][l+1]) - log10(rate[:,0][l]))
-			risefactor = (log10(rate[:,1][mslope]) - log10(rate[:,1][ind]))/(log10(rate[:,0][mslope]) - log10(rate[:,0][ind]))
-			rs.append([name1,alpha,beta,gamma,MBH_Msun2,risefactor])
+			#risefactor = (log10(rate[:,1][mslope]) - log10(rate[:,1][ind]))/(log10(rate[:,0][mslope]) - log10(rate[:,0][ind]))
+			risefactor = (slopes[mslope] - 2.0)/(log10(rate[:,0][mslope + ind]) - log10(rate[:,0][ind]))
+			rs.append([name1,alpha,beta,gamma,MBH_Msun1,risefactor])
 
-		#plt.xlim(1e-1,1)
-		#plt.ylim(1e-11,1e-3)
-	os.chdir('/Users/Natalie/Code/galprocess')
+		plt.xlim(1e-1,1)
+		plt.ylim(1e-11,1e-3)
+		plt.xlabel('$u$',fontsize = 30)
+		plt.ylabel('$d\gamma/d\ln r_p$',fontsize = 30)
+		plt.title('$\mathrm{Rate\,as\,a\,function\,of\,pericenter}$',fontsize = 20)
+		#plt.title('Rates for varying black hole mass')
+	#os.chdir('/Users/Natalie/Code/galprocess')
 	rsa = array(rs)
 	plt.figure()
 	plt.plot(rsa[:,1].astype(float),rsa[:,5].astype(float),'.')
@@ -106,7 +125,10 @@ if __name__ == '__main__':
 	plt.xlabel('$\gamma$',fontsize = 20)
 	plt.ylabel('rise',fontsize = 20)
 	plt.figure()
-	plt.plot(log10(rsa[:,4].astype(float)),rsa[:,5].astype(float),'.')
-	plt.xlabel('$\log_{10}(M_{\mathrm{BH}})$',fontsize = 20)
-	plt.ylabel('rise',fontsize = 20)
+	for n in range(len(rsa)):
+		plt.plot(log10(rsa[:,4][n].astype(float)),rsa[:,5][n].astype(float),'o',color = colors[n],markersize = 12)
+	plt.xlabel('$\log_{10}(M_{\mathrm{BH}})$',fontsize = 25)
+	plt.ylabel('$\mathcal{S}$',fontsize = 30)
+	plt.title('$\mathrm{Rise\,factor\,vs\,black\,hole\,mass}$',fontsize = 20)
+	#plt.savefig('/Users/Natalie/Google Drive/AST425/risefactorNGC3115.png')
 
